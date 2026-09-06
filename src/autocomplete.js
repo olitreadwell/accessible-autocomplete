caef4350d779e33ab01bb258aea06a5a904085c7
olitreadwell <oliver.treadwell@gmail.com>
2026-09-06 08:30:51 +0000

    Only confirm a selected option on blur

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -167,7 +167,10 @@ export default class Autocomplete extends Component {
     let newQuery
     if (this.props.confirmOnBlur) {
       newQuery = newState.query || query
-      this.props.onConfirm(options[selected])
+      const hasSelectedOption = selected !== null && selected >= 0
+      if (hasSelectedOption) {
+        this.props.onConfirm(options[selected])
+      }
     } else {
       newQuery = query
     }
